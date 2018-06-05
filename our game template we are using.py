60dda51b1950184f4cd64502bc9508d2f524a929
ALEXANDER HARRIS <s331394510@ocdsb>
2018-06-05 09:37:52 -0400

    updated game

--- our game template we are using.py
+++ our game template we are using.py
@@ -9,9 +9,9 @@ pygame.init()
 #-------------------------- Colors / Setup Stuff ----------------------------------#
 donald_image = pygame.image.load("donald_trump_8bit.jpg")
 donald = donaldTrump(donald_image, 100)
-
+Background = pygame.image.load("bground.png")
 MenuBackground = pygame.image.load("trump.png")
-
+Levelone = pygame.image.load("level.png")
 WHITE = (255, 255, 255)
 GRAY = (127, 127, 127)
 BLACK = (0, 0, 0)
@@ -57,17 +57,17 @@ def mainMenu_function():
 def settings_function():
     """A function that brings you to the settings screen"""
     global level
-    level = 2
+    level = 4
 #---------------#
 def howTo_function():
     """A function that explains how to play (controls)"""
     global level
-    level = 3
+    level = 2
 #---------------#
 def missionScreen_function():
     """A function that explains the mission (objective of the game)"""
     global level
-    level = 4
+    level = 3
 #---------------#
 def northKorea_function():
     """A function that "fires the nukes" like North Korea"""
@@ -90,23 +90,31 @@ def mousebuttondown(level):
         for button in settings_buttons:
             if button.rect.collidepoint(pos):
                 button.call_back()
+    elif level == 3:
+        for button in settings_buttons:
+            if button.rect.collidepoint(pos):
+                button.call_back()
+    elif level == 4:
+        for button in settings_buttons:
+            if button.rect.collidepoint(pos):
+                button.call_back()
 #----------------------------------------------------------------------------------#
 
 #------------------------------- Button Setup -------------------------------------#
 button_01 = Button("PLAY", (1072, 440), play_function, WHITE)
-button_02 = Button("Main Menu", (SCREENWIDTH/2, SCREENHEIGHT/3), mainMenu_function)
+button_02 = Button("Main Menu", (1072, 770), mainMenu_function)
 button_03 = Button("Quit", (1072, 620), quit_function, RED )
 button_04 = Button("Settings", (1072, 500), settings_function, RED)
-button_05 = Button("How to Play", (1072, 560), howTo_function, WHITE)
-button_06 = Button("Mission Objective", (1072, 680), missionScreen_function, WHITE)
+button_05 = Button("Mission Objective", (1072, 560), howTo_function, WHITE)
+button_06 = Button("How to Play", (1072, 680), missionScreen_function, WHITE)
 button_07 = Button("Nukes Button", (1072, 740), northKorea_function, RED)
 button_08 = Button("Nukes Have Been Fired", (1072, 740), play_function, RED)
 
 #--------------- Button List For Menu ---------------#
 mainMenu_buttons = [button_01, button_03, button_04, button_05, button_06, button_07]
 settings_buttons = [button_02]
-howToPlay_buttons = []
-missionScreen_buttons = []
+howToPlay_buttons = [button_02]
+missionScreen_buttons = [button_02]
 nukesFiredScreen = [button_01, button_03, button_04, button_05, button_06, button_08]
 
 bullet_list = pygame.sprite.Group()
@@ -132,20 +140,51 @@ while carryOn:
 
     # Draw buttons
     if level == 1:
-        MenuBackground = pygame.image.load("trump.png")
         for button in mainMenu_buttons:
             button.draw(screen)
     elif level == 2:
         screen.fill(WHITE)
+        screen.blit(Background, (0,0))
+        for button in howToPlay_buttons:
+            button.draw(screen)
+        font = pygame.font.Font(None, 36)
+        text = font.render("The evil James Comey has caused a zombie appocalyspse in an act of revenge for his firing", 1, (10, 10, 10))
+        screen.blit(text, (50,50))
+        font = pygame.font.Font(None, 36)
+        text = font.render("The Objective of the game is you(Donald Trump) Must eliminate all the zombies ", 1, (10, 10, 10))
+        screen.blit(text, (50,100))
+        font = pygame.font.Font(None, 36)
+        text = font.render("by any means possible, If so much as one zombie remains you will lose and the world will die.", 1, (10, 10, 10))
+        screen.blit(text, (50,150))
+        font = pygame.font.Font(None, 36)
+        text = font.render("The entire fate of the world rests in your orange hands Donald...", 1, (10, 10, 10))
+        screen.blit(text, (50,200))
+        font = pygame.font.Font(None, 36)
+        text = font.render("Good luck and Godspeed", 1, (10, 10, 10))
+        screen.blit(text, (600,250))
+        font = pygame.font.Font(None, 36)
+        text = font.render("-Benjamin Solomon Carson Sr.", 1, (10, 10, 10))
+        screen.blit(text, (600,300))
+    elif level == 3:
+        screen.fill(WHITE)
+        screen.blit(Background, (0,0))
         for button in settings_buttons:
             button.draw(screen)
-    elif level == 5:
+        font = pygame.font.Font(None, 36)
+        text = font.render("The evil James Comey has caused a zombie appocalyspse in an act of revenge for his firing", 1, (10, 10, 10))
+        screen.blit(text, (50,50))
+    elif level == 4:
+        screen.fill(WHITE)
+        screen.blit(Background, (0,0))
+        for button in settings_buttons:
+            button.draw(screen)
+    elif  level == 5:
         for button in nukesFiredScreen:
             button.draw(screen)
     elif level == 8: #Actual Game Screen
+        screen.blit(Levelone, (0,0))
         keys = pygame.key.get_pressed()
         bullet_list.update()
-        screen.fill(WHITE)
         donald.display(screen)
         bullet_list.draw(screen)
         
